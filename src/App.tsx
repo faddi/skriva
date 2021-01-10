b63497fef01e27a977a9b0845a0f90c4957071d1
faddi <faddi.idris@gmail.com>
2021-01-10 20:51:23 +0100

    added more words

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,55 @@ const data: IImageEntry[] = [
       "https://d25tv1xepz39hi.cloudfront.net/2016-07-16/files/cat-sample_1313-2.jpg",
     ],
   },
+  {
+    type: "image",
+    text: "flamingo",
+    imageLinks: [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Wildlife_in_and_around_Reserva_Laguna_Nimez_in_El_Calafate%2C_Argentina_-_Chilaen_Flamingo_%28Phoenicopterus_chilensis%29_-_%2825186867925%29.jpg/1200px-Wildlife_in_and_around_Reserva_Laguna_Nimez_in_El_Calafate%2C_Argentina_-_Chilaen_Flamingo_%28Phoenicopterus_chilensis%29_-_%2825186867925%29.jpg",
+    ],
+  },
+  {
+    type: "image",
+    text: "björn",
+    imageLinks: [
+      "https://www.oysterworldwide.com/panel/wp-content/uploads/2014/02/Project_Romania_Bear_in_woods.jpg",
+    ],
+  },
+  {
+    type: "image",
+    text: "orm",
+    imageLinks: [
+      "https://images.bonnier.cloud/files/ill/production/2020/03/27110839/huggorm.jpg?auto=compress&q=30&fit=crop&crop=focalpoint&fp-x=0.5&fp-y=0.5&w=1920&ixlib=imgixjs-3.4.2",
+    ],
+  },
+  {
+    type: "image",
+    text: "lejon",
+    imageLinks: [
+      "https://cdn.abicart.com/shop/26809/art9/h5932/153985932-origpic-65449f.jpg",
+    ],
+  },
+  {
+    type: "image",
+    text: "snigel",
+    imageLinks: [
+      "https://static.vecteezy.com/ti/gratis-foton/p1/1224348-liten-tradgard-snigel-korsning-gren-gratis-fotona.jpg",
+    ],
+  },
+  {
+    type: "image",
+    text: "skölpadda",
+    imageLinks: [
+      "https://www.lidkopingsnytt.nu/foton/2018/05/sk%C3%B6ldpadda-tobax.jpg",
+    ],
+  },
+  {
+    type: "image",
+    text: "fisk",
+    imageLinks: [
+      "https://www.natursidan.se/wp-content/uploads/2020/10/72_08A4840.jpg",
+    ],
+  },
 ];
 
 function App() {
@@ -47,7 +96,7 @@ function App() {
 
     const val = e.currentTarget.value;
 
-    if (val === test.text) {
+    if (val.toLocaleLowerCase() === test.text.toLocaleLowerCase()) {
       setCorrect(true);
     }
 
@@ -56,6 +105,7 @@ function App() {
 
   const onKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter" && correct) {
+      console.log("asdf");
       setCorrect(false);
       setValue("");
       setIndex(randomIndex(data));
--- src/Entry.tsx
+++ src/Entry.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { EntryType } from "./App";
 import { randomChoice } from "./util";
 
